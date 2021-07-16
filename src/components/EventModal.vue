a34e43eddefefe9495e99907fd0c90a5a6b01f38
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-07-16 15:49:40 +0200

    fixup! Create an event from an email
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/components/EventModal.vue
+++ src/components/EventModal.vue
@@ -2,10 +2,16 @@
 	<Modal @close="onClose">
 		<div class="modal-content">
 			<h2>{{ t('mail', 'Create event') }}</h2>
+			<Multiselect
+				v-model="selectedCalendar"
+				label="displayname"
+				track-by="url"
+				:allow-empty="false"
+				:options="calendars" />
 			<input v-model="eventTitle" type="text">
-			<DatetimePicker></DatetimePicker>
-			<DatetimePicker></DatetimePicker>
-			<button class="primary">
+			<DatetimePicker type="datetime" :show-timezone-select="true" :timezone-id="startTimezoneId" />
+			<DatetimePicker type="datetime" :show-timezone-select="true" :timezone-id="endTimezoneId" />
+			<button class="primary" @click="onSave">
 				{{ t('mail', 'Create') }}
 			</button>
 		</div>
@@ -14,8 +20,11 @@
 
 <script>
 import DatetimePicker from '@nextcloud/vue/dist/Components/DatetimePicker'
+import jstz from 'jstz'
 import Modal from '@nextcloud/vue/dist/Components/Modal'
+import Multiselect from '@nextcloud/vue/dist/Components/Multiselect'
 
+import { getUserCalendars } from '../service/DAVService'
 import logger from '../logger'
 
 export default {
@@ -23,6 +32,7 @@ export default {
 	components: {
 		DatetimePicker,
 		Modal,
+		Multiselect,
 	},
 	props: {
 		envelope: {
@@ -31,8 +41,17 @@ export default {
 		},
 	},
 	data() {
+		// Try to determine the current timezone, and fall back to UTC otherwise
+		const defaultTimezone = jstz.determine()
+		const defaultTimezoneId = defaultTimezone ? defaultTimezone.name() : 'UTC'
+
 		return {
+			calendars: [],
 			eventTitle: this.envelope.subject,
+			startTimezoneId: defaultTimezoneId,
+			endTimezoneId: defaultTimezoneId,
+			saving: false,
+			selectedCalendar: undefined,
 		}
 	},
 	created() {
@@ -40,9 +59,31 @@ export default {
 			envelope: this.envelope,
 		})
 	},
+	async mounted() {
+		this.calendars = (await getUserCalendars()).filter(c => c.writable)
+
+		if (this.calendars.length) {
+			this.selectedCalendar = this.calendars[0]
+		}
+	},
 	methods: {
 		onClose() {
 			this.$emit('close')
+		},
+		async onSave() {
+			this.saving = true
+
+			try {
+				console.info('create event', {
+					calendar: this.selectedCalendar,
+					eventTitle: this.eventTitle,
+					startDate: this.startDate,
+					startTimezone: this.startTimezoneId,
+					endTimezone: this.endTimezoneId,
+				})
+			} finally {
+				this.saving = false
+			}
 		},
 	},
 }
